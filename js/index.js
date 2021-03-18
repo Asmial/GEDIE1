96867275e5c77ec9b894630b86858593c1b90d48
Asmial <asiermiguel@gmail.com>
2021-03-18 17:41:49 +0100

    reproductor muy básico

--- js/index.js
+++ js/index.js
@@ -1,3 +1,59 @@
 const $ = require('jquery');
 window['jQuery'] = window['$'] = $;
 var bootstrap = require('bootstrap')
+
+
+
+
+
+jQuery(() => {
+
+    /** @type HTMLVideoElement */
+    // @ts-ignore
+    const video = $('#video').get(0);
+    const videoControls = $('#video-controls');
+    const playButton = $('#play');
+
+    $(document).on('keypress', function (e) {
+        switch (e.key) {
+            case ' ':
+            case 'k':
+                togglePlay();
+                break;
+            default:
+                console.log('has pulsado ' + e.key);
+        }
+    })
+
+    const videoWorks = !!document.createElement('video').canPlayType;
+    if (videoWorks) {
+        video.controls = false;
+        videoControls.removeClass('hidden');
+    }
+
+    function togglePlay() {
+        if (video.paused || video.ended) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    }
+
+    playButton.on('click', togglePlay);
+
+    $(video).on('play pause', () => {
+        var icon = playButton.children()
+        if (video.paused) {
+            playButton.attr('data-title', 'Reproducir (k)')
+            icon.removeClass("fa-pause");
+            icon.addClass("fa-play");
+        } else {
+            playButton.attr('data-title', 'Pausar (k)')
+            icon.removeClass("fa-play")
+            icon.addClass("fa-pause");
+        }
+    });
+
+})
+
+
